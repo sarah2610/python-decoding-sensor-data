c689927981cdd5c009e1ba4422a221b8c4578b95
sarah2610 <67005823+sarah2610@users.noreply.github.com>
2023-01-03 15:17:24 +0530

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,5 +1,6 @@
 # Runner script for all modules
 
+from load_data import load_sensor_data
 
 ##############################
 # Do not remove these two lines
@@ -9,11 +10,15 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
-
+data = load_sensor_data()
+print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
 # Module 3 code here:
 
 # Module 4 code here:
 
+# Module 5 code here:
+
+# Module 6 code here:
 # Module 5 code here:
